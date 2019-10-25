e5f112b648a9d89b7c07d26aa93f547135304e67
Sadia Badhon <sadia.islam.badhon@gmail.com>
2019-10-25 05:00:31 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -16,10 +16,11 @@ def load_library(file_path)
   new_hash
 end
 
-def get_japanese_emoticon
-  # code goes here
+def get_japanese_emoticon(file_path, emoticon)
+  result = load_library(file_path)['get_emoticon'][emoticon]
+  result ? result : "Sorry, that emoticon was not found"
 end
 
-def get_english_meaning
-  # code goes here
-end
+def get_english_meaning(file_path, emoticon)
+  result = load_library(file_path)['get_meaning'][emoticon]
+  result ? result : "Sorry, that emoticon was not found"
